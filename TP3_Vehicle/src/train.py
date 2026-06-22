8b7fdc7f5bd49de42bd3f5ded9d82f9e868bd747
Adil SOUDRI <adilsoudri@gmail.com>
2026-06-22 16:11:22 +0100

    Met à jour le suivi des fichiers selon .gitignore

--- TP3_Vehicle/src/train.py
+++
@@ -1,9 +0,0 @@
-def train_model(model, X_train, y_train):
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=50,
-        validation_split=0.2,
-        verbose=1
-    )
-    return history
--- TP3_Vehicle/src/utils.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import mean_absolute_error
-
-def evaluate_model(y_true, y_pred):
-    mae = mean_absolute_error(y_true, y_pred)
-    print("MAE:", mae)
-    return mae
